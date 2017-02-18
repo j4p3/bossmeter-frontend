c01c69572c9d9d6c1010b7fd83954df8fd20ebaf
JP <bonnerjp@gmail.com>
2017-02-18 03:16:27 -0500

    index design

--- src/components/main.js
+++ src/components/main.js
@@ -67,7 +67,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <h1 style={{align: 'center', em: 2,}}>MOODMETER {this.state.name ? ':' + this.state.name : ''}</h1>
+      <h2 style={{align: 'center', em: 2,}}>{this.state.name ? 'MOODMETER FOR ' + this.state.name : ''}</h2>
       <div className="App row" style={{display: 'flex', flexDirection: 'row', marginTop: "40px"}}>
 
         <div style={{flex: 2}}>
